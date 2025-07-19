44de6c10429684ea5a178f28a6b29f4b79a529bb
Alexisp1 <35416828+Alexisp1@users.noreply.github.com>
2025-07-18 21:42:51 -0500

    Complete revert to original state

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,17 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar'
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
-import { ThemeProvider } from '@/contexts/ThemeContext';
 import React from 'react';
 
 export default function RootLayout() {
   useFrameworkReady();
   
   return (
-    <ThemeProvider>
+    <>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
