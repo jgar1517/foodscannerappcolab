e6808dd254fb9ce6a72a6e939e3732f538d5a97e
Alexisp1 <35416828+Alexisp1@users.noreply.github.com>
2025-07-18 20:47:51 -0500

    Update layout with correct PaperProvider import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,14 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar'
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
-import { Provider as PaperProvider, DefaultTheme, DarkTheme } from 'react-native-paper';
-import { useState } from 'react';
+import { PaperProvider } from 'react-native-paper';
 import React from 'react';
 
 export default function RootLayout() {
   useFrameworkReady();
-  const [isDark, setIsDark] = useState(false);
   
   return (
-    <PaperProvider theme={isDark ? DarkTheme : DefaultTheme}>
+    <PaperProvider>
       <>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="+not-found" />
